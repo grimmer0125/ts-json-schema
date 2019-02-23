44d339c33f216b34fbb215bd8d2cc600ed15a090
Grimmer <grimmer0125@gmail.com>
2019-02-23 22:54:22 +0800

    Add ajv to use json-schema to do validating

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 JS-JSON-SCHEMA
 --------------
 
-## Command line 
+## Prerequisite
+
+1. Install yarn. https://yarnpkg.com/zh-Hans/
+1. Install TypeScript globally [recommended way]. `yarn add global typescript`
+2. Execute `yarn` or `yarn install` in project root. 
+
+## Command line usage
 
 `typescript-json-schema myString.ts MyString`
 
-## Programming
+## Programming usage
 
+1. Compile TypeScript to JavaScript, `tsc example.ts`
+2. Test, `node example.js`
+
+## TODO
+
+1. Test required/optional field case
+2. Test https://quicktype.io/, GraphQL -> json-schema
+3. Import json-schema in other language. 
---
+++ example.ts
@@ -0,0 +1,50 @@
+const path = require('path');
+const TJS  = require("typescript-json-schema")
+const fs = require('fs');
+
+// import * as Ajv from "ajv";
+const Ajv = require('ajv');
+
+// optionally pass ts compiler options
+const compilerOptions = {
+    strictNullChecks: true
+}
+
+// optionally pass argument to schema generator
+const settings = {
+    required: true
+};
+
+const files = [path.resolve("myString.ts")];
+const schema = TJS.generateSchema(TJS.getProgramFromFiles(files, compilerOptions), "MyString", settings);
+console.log("json-schema:", schema);
+// ajv.validateSchema(actual);
+
+// Try to export to a schema.json & import 
+const filename = './schema.json';
+fs.writeFileSync(filename, JSON.stringify(schema));
+const schemaStr = fs.readFileSync(filename, "utf8");
+const schema2 = JSON.parse(schemaStr)
+
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+const validate = ajv.compile(schema2);
+
+console.log("test1-string")
+let data = "randomStr";
+let valid = validate(data);
+if (!valid)  {
+    console.log(validate.errors);
+} else {
+    console.log("ok");
+}
+
+console.log("test2-number")
+let data2 = 0;
+let valid2 = validate(data2);
+if (!valid2)  {
+    console.log(validate.errors);
+} else {
+    console.log("ok");
+}
+
+
